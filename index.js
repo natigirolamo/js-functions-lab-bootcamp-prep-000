2f8286d76fc3f0f855a0ade6b1cc2bda3b3db238
exuberant-glitch-6816 <natalia.girolamo@gmail.com>
2019-05-05 00:54:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // write your code below!
  function happyHolidays() {
    console.log("Happy Holidays") 
- }
+ };
  
  function happyHolidaysTo (name) {
    console.log(`Happy holidays, ${name}!`)
-}
- }
+};
+ 
+ function happyHolidayTo (holiday, name)
+ return `${holiday}, ${name}!`
+ };
+ 
+ 
  
  
